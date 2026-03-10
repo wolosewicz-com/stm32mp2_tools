ff4dd06595487a0b0cacef0df831ab89bb3aaec7
Maciej Wołosewicz <maciej@wolosewicz.com>
2026-03-10 12:06:13 +0100

    Fix Vite base path to match repo name (underscore not hyphen)

--- bsec-lifecycle/vite.config.js
+++ bsec-lifecycle/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/stm32mp2-tools/bsec-lifecycle/',
+  base: '/stm32mp2_tools/bsec-lifecycle/',
 })
